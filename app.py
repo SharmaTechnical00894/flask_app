9ee677ca699710c2e70885e1008597ba5456d7b1
SharmaTechnical00894 <thesharmatechnical@gmail.com>
2025-05-04 23:32:57 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -6,6 +6,19 @@ app = Flask(__name__)
 def homePage():
     return "Hello World"
 
+@app.route("/employee")
+def getEmployee():
+    return {
+        'name':"Akash",
+        'age':36
+    }
+
+@app.route("/<int:a>/<int:b>")
+def addNumber(a,b):
+    return {
+        'inputNumbers': f"{a} and {b}",
+        'addition': a+b
+    }
 
 if(__name__ == "__main__"):
     app.run()
